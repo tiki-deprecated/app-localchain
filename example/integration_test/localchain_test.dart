edbac1ef253bd4dc1d7b5540219c8e954d5dab01
Mike Audi <mike@mytiki.com>
2022-02-10 06:09:22 -0600

    added localchain api & corresponding tests
    fixes #5 & #7

--- example/integration_test/localchain_test.dart
+++ example/integration_test/localchain_test.dart
@@ -3,12 +3,49 @@
  * MIT license. See LICENSE file in root directory.
  */
 
+import 'dart:typed_data';
+
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
+import 'package:localchain/localchain.dart';
+import 'package:uuid/uuid.dart';
 
 void main() {
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
+
   group('Localchain Tests', () {
-    test('test 1', () async {});
+    test('open_success', () async {
+      await Localchain().open(Uuid().v4());
+    });
+
+    test('append_success', () async {
+      Localchain localchain = await Localchain().open(Uuid().v4());
+      Uint8List contents =
+          Localchain.codec.encode(BlockContentsJson(json: '"hello":"world'));
+      Block block = await localchain.append(contents);
+      expect(block.created != null, true);
+      expect(block.previousHash != null, true);
+      expect(block.contents, contents);
+    });
+
+    test('get_success', () async {
+      Localchain localchain = await Localchain().open(Uuid().v4());
+      Uint8List contents =
+          Localchain.codec.encode(BlockContentsJson(json: '"hello":"world'));
+      for (int i = 0; i < 200; i++) await localchain.append(contents);
+      List<Block> blocks = await localchain.get(onPage: (page) {
+        expect(page.length > 0, true);
+      });
+      expect(blocks.length, 201);
+    });
+
+    test('validate_success', () async {
+      Localchain localchain = await Localchain().open(Uuid().v4());
+      Uint8List contents =
+          Localchain.codec.encode(BlockContentsJson(json: '"hello":"world'));
+      for (int i = 0; i < 200; i++) await localchain.append(contents);
+      bool isValid = await localchain.validate();
+      expect(isValid, true);
+    });
   });
 }
--- lib/localchain.dart
+++ lib/localchain.dart
@@ -3,21 +3,81 @@
  * MIT license. See LICENSE file in root directory.
  */
 
-export 'src/block/block_model.dart';
-export 'src/block/block_service.dart';
+import 'dart:typed_data';
+
+import 'package:localchain/src/db/db_model_page.dart';
+import 'package:logging/logging.dart';
+import 'package:sqflite_sqlcipher/sqflite.dart';
+
+import 'src/block/block.dart';
+import 'src/block/block_model.dart';
+import 'src/block/block_service.dart';
+import 'src/block/contents/block_contents_codec.dart';
+import 'src/db/db.dart' as db;
+
+export 'src/block/block.dart';
 export 'src/block/contents/block_contents.dart';
 export 'src/block/contents/block_contents_bytea.dart';
 export 'src/block/contents/block_contents_codec.dart';
 export 'src/block/contents/block_contents_data_nft.dart';
 export 'src/block/contents/block_contents_json.dart';
+export 'src/block/contents/block_contents_schema.dart';
 export 'src/block/contents/block_contents_start.dart';
 export 'src/block/contents/block_contents_uri_nft.dart';
-export 'src/db/db.dart';
-export 'src/db/db_model_page.dart';
 
-// construct
-// (Factory style) open (which should open db & call back when done?)
-// getBlock/(s)
-// addBlock
-// validate chain
-// validate block
+class Localchain {
+  final Logger _log = Logger('localchain');
+  late final BlockService _blockService;
+
+  Future<Localchain> open(String address,
+      {String? password, Duration? validate}) async {
+    Database database = await db.open(address,
+        password: password, validate: validate ?? Duration(days: 30));
+    _blockService = BlockService(database);
+    return this;
+  }
+
+  static BlockContentsCodec get codec => contentsCodec;
+
+  Future<Block> append(Uint8List contents) async {
+    BlockModel model = await _blockService.add(contents);
+    return Block(
+        contents: model.contents,
+        previousHash: model.previousHash,
+        created: model.created);
+  }
+
+  Future<bool> validate({int pageSize = 100}) async {
+    try {
+      await _blockService.validate(pageSize: pageSize);
+      return true;
+    } catch (error) {
+      _log.severe('failed to validate localchain', error);
+      return false;
+    }
+  }
+
+  Future<List<Block>> get(
+      {int pageSize = 100, void Function(List<Block>)? onPage}) async {
+    int pageNum = 0;
+    int totalPages = 1;
+    List<Block> chain = List.empty(growable: true);
+    while (pageNum < totalPages) {
+      DbModelPage<BlockModel> page =
+          await _blockService.page(pageNum, pageSize);
+      if (page.elements != null) {
+        List<Block> blocks = page.elements!
+            .map((block) => Block(
+                contents: block.contents,
+                previousHash: block.previousHash,
+                created: block.created))
+            .toList();
+        if (onPage != null) onPage(blocks);
+        chain.addAll(blocks);
+      }
+      pageNum++;
+      totalPages = page.totalPages ?? pageNum;
+    }
+    return chain;
+  }
+}
